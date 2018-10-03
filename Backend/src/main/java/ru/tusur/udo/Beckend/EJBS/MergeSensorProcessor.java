dda40c93da9826220fad73f04e31279de31da9ad
turlyunef <turlyunef@mail.ru>
2018-10-03 15:48:16 +0700

    Корректировка

--- Backend/src/main/java/ru/tusur/udo/Beckend/EJBS/MergeSensorProcessor.java
+++ Backend/src/main/java/ru/tusur/udo/Beckend/EJBS/MergeSensorProcessor.java
@@ -1,10 +1,13 @@
 package ru.tusur.udo.Beckend.EJBS;
 
 import java.util.logging.Logger;
-
+import javax.ejb.Stateless;
+import javax.inject.Named;
 import org.apache.camel.Exchange;
 import org.apache.camel.Processor;
 
+@Stateless
+@Named("mergeSensorProcessor")
 public class MergeSensorProcessor implements Processor{
 	
 	private static final Logger log = Logger.getLogger(MergeSensorProcessor.class.toString());
